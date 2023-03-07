0fe54b8cc99163629e63dea3c8a650b49b4a1e28
tigrexz <95867661+tigrexz@users.noreply.github.com>
2023-03-07 16:03:28 -0700

    Update keywords_processor.py
    Added Argparse function to use filename as argument

--- keywords_processor.py
+++ keywords_processor.py
@@ -1,17 +1,30 @@
 import re
-filename = 'keywords.txt'
+import argparse
+# create argument parser
+parser = argparse.ArgumentParser()
+# add arguements
+parser.add_argument('-f', '--filename', dest='f', type=str, required=True, help="Input file's filename")
 
+# parse arguments
+args = parser.parse_args()
+
+
+filename = str(args.f)
 reg1 = re.compile(r'(\w+\s?\w+)[\n\r,:\.]*')    # works for text with one line or mutiple lines
 
-with open(filename) as f:
-    keywords = f.read()
+try:
+    with open(filename) as file:
+        keywords = file.read()
 
 
-mo1 = reg1.findall(keywords)
-print(mo1)
+    mo1 = reg1.findall(keywords)
+    print('\n')
+    print("the keywords need to be processed are:\n", keywords)
 
-str = ''
-for i in mo1:
-    str += f'\"{i}\" OR '
-#print(str)
-print(str.rstrip(' OR'))
+    str = ''
+    for i in mo1:
+        str += f'\"{i}\" OR '
+    #print(str)
+    print("\nThe processed keywords are here:\n",str.rstrip(' OR'))
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exit!")
