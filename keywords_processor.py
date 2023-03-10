851b4fce8c6ded2eaacde5585d041cb98145eb04
tigrexz <95867661+tigrexz@users.noreply.github.com>
2023-03-09 23:32:09 -0700

    Update keywords_processor.py
    added main() function

--- keywords_processor.py
+++ keywords_processor.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 import re
 import argparse
-# create argument parser
-parser = argparse.ArgumentParser()
-# add arguements
-parser.add_argument('-f', '--filename', dest='f', type=str, required=True, help="Input file's filename")
 
-# parse arguments
-args = parser.parse_args()
+def main():
+    # create argument parser
+    parser = argparse.ArgumentParser()
+    # add arguements
+    parser.add_argument('-f', '--filename', dest='f', type=str, required=True, help="Input file's filename")
 
-# create file object
-filename = str(args.f)
-#create regex object
-reg1 = re.compile(r'(\w+[^\S\r\n]?\w+)[\n\r,:\."\']*')    # works for text with one line or mutiple lines
+    # parse arguments
+    args = parser.parse_args()
 
-try:
-    with open(filename) as file:
-        keywords = file.read()
-#create match object
-    mo1 = reg1.findall(keywords)
+    # create file object
+    filename = str(args.f)
+    # create regex object
+    reg1 = re.compile(r'(\w+[^\S\r\n]?\w+)[\n\r,:\."\']*')  # works for text with one line or mutiple lines
 
-    str = ''
-    for i in mo1:
-        str += f'\"{i}\" OR '
+    try:
+        with open(filename) as file:
+            keywords = file.read()
+        # create match object
+        mo1 = reg1.findall(keywords)
+
+        str = ''
+        for i in mo1:
+            str += f'\"{i}\" OR '
+
+        print('\n')
+        print("The keywords that need to be processed are:\n\n", keywords)
+        print("\nThe processed keywords are:\n\n", str.rstrip(' OR'))
+        print('\n')
+    except FileNotFoundError:
+        print(f"Sorry, {filename} does not exist!")
         
-    print('\n')
-    print("The keywords that need to be processed are:\n\n",keywords) 
-    print("\nThe processed keywords are:\n\n",str.rstrip(' OR'))
-    print('\n')
-except FileNotFoundError:
-    print(f"Sorry, {filename} does not exist!")
+        
+if __name__ == "__main__":
+    main()
