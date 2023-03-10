24c9e56109ecff1c96d389d73a83424fbb72e47f
tigrexz <95867661+tigrexz@users.noreply.github.com>
2023-03-10 14:44:34 -0700

    Update keywords_processor.py

--- keywords_processor.py
+++ keywords_processor.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
+
 import re
 import argparse
 
 def main():
     # create argument parser
     parser = argparse.ArgumentParser()
-    # add arguements
-    parser.add_argument('-f', '--filename', dest='f', type=str, required=True, help="Input file's filename")
+    parser.add_argument('-f', dest='filename', type=str, required=True, help="Keywords file")
 
     # parse arguments
     args = parser.parse_args()
 
-    # create file object
-    filename = str(args.f)
-    # create regex object
-    reg1 = re.compile(r'(\w+[^\S\r\n]?\w+)[\n\r,:\."\']*')  # works for text with one line or mutiple lines
+    # create regex which works for text with one line or mutiple lines
+    keyword_regex = re.compile(r'(\w+[^\S\r\n]?\w+)[\n\r,:\."\']*')
 
     try:
-        with open(filename) as file:
+        with open(args.filename) as file:
             keywords = file.read()
-        # create match object
-        mo1 = reg1.findall(keywords)
 
-        str1 = ''
-        for i in mo1:
-            str += f'\"{i}\" OR '
+        # find the matches
+        matches = ''
+        match = keyword_regex.findall(keywords)
+        for keyword in match:
+            matches += '"{}" OR '.format(keyword)
 
-        print('\n')
         print("The keywords that need to be processed are:\n\n", keywords)
-        print("\nThe processed keywords are:\n\n", str1.rstrip(' OR'))
-        print('\n')
+        print("The processed keywords are:\n\n", matches.rstrip(' OR'))
     except FileNotFoundError:
-        print(f"Sorry, {filename} does not exist!")
+        print("Keyword file '{}' does not exist.".format(args.filename))
 
 
 if __name__ == "__main__":
